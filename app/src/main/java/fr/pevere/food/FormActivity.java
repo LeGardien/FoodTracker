08324d71966b9aadef643d88db7981eb102b6dc7
Matthieu PEVERE <matthieupevere@berkeley.edu>
2022-10-28 09:29:18 -0700

    Welcome activity and add meal activity

---
+++ app/src/main/java/fr/pevere/food/FormActivity.java
@@ -0,0 +1,32 @@
+package fr.pevere.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FormActivity extends AppCompatActivity {
+
+
+    private TextView mTitleTextView;
+    private TextView mQuestionWhenTextView;
+    private TextView mQuestionWhatTextView;
+    private TextView mQuestionHowMuchTextView;
+    private EditText mQuestionWhenText;
+    private EditText mQuestionWhatText;
+    private EditText mQuestionHowMuchText;
+    private Button mNextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        mTitleTextView = findViewById(R.id.game_activity_textview_question);
+        mQuestionWhenTextView = findViewById(R.id.game_activity_textview_question2);
+        mQuestionWhatTextView = findViewById(R.id.game_activity_textview_question3);
+        mQuestionHowMuchTextView = findViewById(R.id.game_activity_textview_question4);
+    }
+}
